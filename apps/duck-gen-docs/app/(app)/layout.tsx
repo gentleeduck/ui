7e0da80669833eccf96204e9fb6dd2da06f598f5
wildduck2 <wezonaser50@gmail.com>
2026-02-13 10:52:50 +0200

    refactor(layout): simplify app wrapper containers

--- apps/duck-gen-docs/app/(app)/layout.tsx
+++ apps/duck-gen-docs/app/(app)/layout.tsx
@@ -1,9 +1,8 @@
-'use client'
 import { SiteFooter, SiteHeader } from '@gentleduck/docs/client'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col place-content-center items-center" data-wrapper="">
+    <div className="flex min-h-screen flex-col" data-wrapper="">
       <SiteHeader />
       <main className="flex w-full flex-1 flex-col">{children}</main>
       <SiteFooter />
--- apps/www/app/(app)/layout.tsx
+++ apps/www/app/(app)/layout.tsx
@@ -1,10 +1,9 @@
-'use client'
 import { SiteFooter } from '@gentleduck/docs/client'
 import { SiteHeader } from '~/components/site-header'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col place-content-center items-center" data-wrapper="">
+    <div className="flex min-h-screen flex-col" data-wrapper="">
       <SiteHeader />
       <main className="flex w-full flex-1 flex-col">{children}</main>
       <SiteFooter />
