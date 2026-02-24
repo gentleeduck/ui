4ba61d611d998662198a0a25eba8424ec9162bdc
wildduck2 <wezonaser50@gmail.com>
2026-02-24 09:45:13 +0200

    feat(pagination): add Arabic RTL example and update docs
    Update pagination-2 example with Arabic text props for Previous, Next,
    and Ellipsis labels plus Arabic-Indic page numbers. Document the text
    prop in the MDX API reference for all three components.

--- packages/registry-examples-duckui/src/pagination/pagination-2.tsx
+++ packages/registry-examples-duckui/src/pagination/pagination-2.tsx
@@ -10,28 +10,35 @@ import {
 
 export default function PaginationRtlDemo() {
   return (
-    <div dir="rtl">
+    <div dir="rtl" className="flex justify-center">
       <Pagination>
         <PaginationContent>
+          {/* In RTL: Next comes first (visually right side) */}
           <PaginationItem>
-            <PaginationPrevious href="#" />
+            <PaginationNext href="#" text={'\u0627\u0644\u062A\u0627\u0644\u064A'} />
           </PaginationItem>
+
           <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
+            <PaginationEllipsis text={'\u0635\u0641\u062D\u0627\u062A \u0623\u062E\u0631\u0649'} />
           </PaginationItem>
+
+          <PaginationItem>
+            <PaginationLink href="#">{'\u0663'}</PaginationLink>
+          </PaginationItem>
+
           <PaginationItem>
             <PaginationLink href="#" isActive>
-              2
+              {'\u0662'}
             </PaginationLink>
           </PaginationItem>
+
           <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
+            <PaginationLink href="#">{'\u0661'}</PaginationLink>
           </PaginationItem>
+
+          {/* Previous goes at the end (visually left side) */}
           <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href="#" />
+            <PaginationPrevious href="#" text={'\u0627\u0644\u0633\u0627\u0628\u0642'} />
           </PaginationItem>
         </PaginationContent>
       </Pagination>
