536af2bc711d98ec04b3b226c4a5a16e0a709cdd
wildduck2 <wezonaser50@gmail.com>
2026-02-26 05:05:50 +0200

    feat(direction): add DOM wrapper to DirectionProvider and update RTL support
    DirectionProvider now renders a div with dir and style attributes.
    Update direction re-exports and add RTL translate classes to switch.

--- packages/registry-ui-duckui/src/direction/direction.tsx
+++ packages/registry-ui-duckui/src/direction/direction.tsx
@@ -1,2 +1,13 @@
 'use client'
-export * from '@gentleduck/primitives/direction'
+
+import {
+  DIRECTION_DICTIONARY,
+  type Direction,
+  DirectionContext,
+  DirectionProvider,
+  type DirectionProviderProps,
+  Provider,
+  useDirection,
+} from '@gentleduck/primitives/direction'
+export type { Direction, DirectionProviderProps }
+export { useDirection, Provider, DirectionProvider, DirectionContext, DIRECTION_DICTIONARY }
