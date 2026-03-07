08a19f3f634d0996fd6bcdd20fe17415012d0054
wildduck2 <wezonaser50@gmail.com>
2026-03-07 13:10:54 +0200

    fix(docs): fix the overflow

--- apps/duck-ui-docs/app/(app)/layout.tsx
+++ apps/duck-ui-docs/app/(app)/layout.tsx
@@ -2,7 +2,7 @@ import { SiteFooter, SiteHeader } from '@gentleduck/docs/client'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col overflow-hidden" data-wrapper="">
+    <div className="flex min-h-screen flex-col" data-wrapper="">
       <a
         className="sr-only focus:not-sr-only focus:fixed focus:z-50 focus:m-3 focus:rounded-md focus:bg-background focus:px-4 focus:py-2 focus:font-medium focus:text-sm focus:shadow-md focus:ring-2 focus:ring-ring"
         href="#main-content">
