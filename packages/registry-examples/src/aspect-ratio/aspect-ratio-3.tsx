eb146f3391f85606e198a30000dd50511883194e
wildduck <108896341+wildduck2@users.noreply.github.com>
2026-04-06 23:46:21 +0200

    feat(aspect-ratio): add MotionAspectRatio with scaleBlur animation

---
+++ packages/registry-examples/src/aspect-ratio/aspect-ratio-3.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { MotionAspectRatio } from '@gentleduck/registry-ui/aspect-ratio'
+import Image from 'next/image'
+
+export default function Demo() {
+  return (
+    <MotionAspectRatio className="rounded-lg bg-muted" ratio={'16/9'}>
+      <Image
+        alt="Photo by Drew Beamer"
+        className="rounded-lg object-cover dark:grayscale"
+        height={450}
+        src="https://plus.unsplash.com/premium_photo-1672116453000-c31b150f48ef?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        width={800}
+      />
+    </MotionAspectRatio>
+  )
+}
