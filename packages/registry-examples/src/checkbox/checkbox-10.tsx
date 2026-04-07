601b2cbf1f67fc0a47ee5630a0341641e326af3f
wildduck <108896341+wildduck2@users.noreply.github.com>
2026-04-07 10:33:56 +0200

    fix(checkbox): use unique IDs in checkbox-10 example to avoid conflicts

--- packages/registry-examples/src/checkbox/checkbox-10.tsx
+++ packages/registry-examples/src/checkbox/checkbox-10.tsx
@@ -5,17 +5,22 @@ import { MotionCheckboxWithLabel } from '@gentleduck/registry-ui/checkbox'
 export default function Demo() {
   return (
     <div className="flex flex-col gap-3">
-      <MotionCheckboxWithLabel id="terms" _checkbox={{}} _label={{ children: 'Accept terms and conditions' }} index={0} />
       <MotionCheckboxWithLabel
-        id="updates"
+        id="motion-privacy"
+        _checkbox={{}}
+        _label={{ children: 'Accept privacy policy' }}
+        index={0}
+      />
+      <MotionCheckboxWithLabel
+        id="motion-newsletter"
         _checkbox={{ defaultChecked: true }}
-        _label={{ children: 'Send me updates' }}
+        _label={{ children: 'Subscribe to newsletter' }}
         index={1}
       />
       <MotionCheckboxWithLabel
-        id="marketing"
+        id="motion-analytics"
         _checkbox={{}}
-        _label={{ children: 'Receive marketing emails' }}
+        _label={{ children: 'Allow usage analytics' }}
         index={2}
       />
     </div>
