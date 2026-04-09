36ec92680da9469135d34225cc89a46289f2da0d
wildduck2 <wezonaser50@gmail.com>
2026-04-10 01:17:26 +0200

    style(registry-ui): reduce default toggle padding from px-4 to px-3

--- packages/registry-ui/src/toggle/toggle.constants.ts
+++ packages/registry-ui/src/toggle/toggle.constants.ts
@@ -9,7 +9,7 @@ export const toggleVariants = cva(
     },
     variants: {
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
+        default: 'h-9 px-3 py-2 has-[>svg]:px-3',
         lg: 'h-10 min-w-11 px-6 has-[>svg]:px-4',
         sm: 'h-8 min-w-9 gap-1.5 px-3 has-[>svg]:px-2.5',
       },
