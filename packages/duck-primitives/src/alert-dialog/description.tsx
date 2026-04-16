8f39fa3a718a4fc4c1d2b740cdd81dfe6c216c69
wildduck <108896341+wildduck2@users.noreply.github.com>
2026-04-16 12:54:00 +0000

    refactor(primitives): finish alert-dialog namespace migration
    Complete alert-dialog subfile rewrites missed in prior commit
    (alert-dialog.tsx root + description, overlay, portal, title, trigger).
    All import IAlertDialog namespace and use IScoped<T> instead of local
    ScopedProps type.

--- packages/duck-primitives/src/alert-dialog/description.tsx
+++ packages/duck-primitives/src/alert-dialog/description.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react'
 import * as DialogPrimitive from '../dialog'
-import { type ScopedProps, useDialogScope } from './alert-dialog'
+import { useDialogScope } from './alert-dialog'
+import type { IAlertDialog } from './alert-dialog.types'
 
 const DESCRIPTION_NAME = 'AlertDialogDescription'
 
 type AlertDialogDescriptionElement = React.ComponentRef<typeof DialogPrimitive.Description>
-type DialogDescriptionProps = React.ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
-export interface IAlertDialogDescriptionProps extends DialogDescriptionProps {}
 
 /** Accessible description for the alert dialog content. */
-export const AlertDialogDescription = React.forwardRef<AlertDialogDescriptionElement, IAlertDialogDescriptionProps>(
-  (props: ScopedProps<IAlertDialogDescriptionProps>, forwardedRef) => {
+export const AlertDialogDescription = React.forwardRef<AlertDialogDescriptionElement, IAlertDialog.IDescriptionProps>(
+  (props: IAlertDialog.IScoped<IAlertDialog.IDescriptionProps>, forwardedRef) => {
     const { __scopeAlertDialog, ...descriptionProps } = props
     const dialogScope = useDialogScope(__scopeAlertDialog)
     return <DialogPrimitive.Description {...dialogScope} {...descriptionProps} ref={forwardedRef} />
--- packages/duck-primitives/src/alert-dialog/portal.tsx
+++ packages/duck-primitives/src/alert-dialog/portal.tsx
@@ -1,14 +1,12 @@
 import type * as React from 'react'
 import * as DialogPrimitive from '../dialog'
-import { type ScopedProps, useDialogScope } from './alert-dialog'
+import { useDialogScope } from './alert-dialog'
+import type { IAlertDialog } from './alert-dialog.types'
 
 const PORTAL_NAME = 'AlertDialogPortal'
 
-type DialogPortalProps = React.ComponentPropsWithoutRef<typeof DialogPrimitive.Portal>
-export interface IAlertDialogPortalProps extends DialogPortalProps {}
-
 /** Renders alert dialog content into a React portal. */
-const AlertDialogPortal: React.FC<IAlertDialogPortalProps> = (props: ScopedProps<IAlertDialogPortalProps>) => {
+const AlertDialogPortal: React.FC<IAlertDialog.IPortalProps> = (props: IAlertDialog.IScoped<IAlertDialog.IPortalProps>) => {
   const { __scopeAlertDialog, ...portalProps } = props
   const dialogScope = useDialogScope(__scopeAlertDialog)
   return <DialogPrimitive.Portal {...dialogScope} {...portalProps} />
