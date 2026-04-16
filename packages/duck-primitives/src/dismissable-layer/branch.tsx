e6ae3461b4936941a9d48467655478f8f098e0d9
wildduck <108896341+wildduck2@users.noreply.github.com>
2026-04-16 16:21:00 +0000

    refactor(primitives/dismissable-layer): namespace dismissable-layer types
    Extract IDismissableLayer namespace with props + event types.
    Drop flat IDismissableLayer*Props, FocusOutsideEvent, PointerDownOutsideEvent
    re-exports from the barrel.

--- packages/duck-primitives/src/dismissable-layer/branch.tsx
+++ packages/duck-primitives/src/dismissable-layer/branch.tsx
@@ -2,14 +2,13 @@ import * as React from 'react'
 import { useComposedRefs } from '../libs/compose-ref'
 import { Primitive } from '../primitive-elements'
 import { DismissableLayerContext } from './dismissable-layer'
+import type { IDismissableLayer } from './dismissable-layer.types'
 
 const BRANCH_NAME = 'DismissableLayerBranch'
 
 type DismissableLayerBranchElement = React.ComponentRef<typeof Primitive.div>
-type PrimitiveDivProps = React.ComponentPropsWithoutRef<typeof Primitive.div>
-export interface IDismissableLayerBranchProps extends PrimitiveDivProps {}
 
-export const DismissableLayerBranch = React.forwardRef<DismissableLayerBranchElement, IDismissableLayerBranchProps>(
+export const DismissableLayerBranch = React.forwardRef<DismissableLayerBranchElement, IDismissableLayer.IBranchProps>(
   (props, forwardedRef) => {
     const context = React.useContext(DismissableLayerContext)
     const ref = React.useRef<DismissableLayerBranchElement>(null)
