c93bf525e673de20df8d9503d2e854c8574b4594
wildduck <108896341+wildduck2@users.noreply.github.com>
2026-04-17 22:43:55 +0200

    refactor(primitives): finish alert-dialog namespace migration
    Complete alert-dialog subfile rewrites missed in prior commit
    (alert-dialog.tsx root + description, overlay, portal, title, trigger).
    All import IAlertDialog namespace and use IScoped<T> instead of local
    ScopedProps type.

--- packages/duck-primitives/src/alert-dialog/overlay.tsx
+++ packages/duck-primitives/src/alert-dialog/overlay.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react'
 import * as DialogPrimitive from '../dialog'
-import { type ScopedProps, useDialogScope } from './alert-dialog'
+import { useDialogScope } from './alert-dialog'
+import type { IAlertDialog } from './alert-dialog.types'
 
 const OVERLAY_NAME = 'AlertDialogOverlay'
 
 type AlertDialogOverlayElement = React.ComponentRef<typeof DialogPrimitive.Overlay>
-type DialogOverlayProps = React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
-export interface IAlertDialogOverlayProps extends DialogOverlayProps {}
 
 /** Overlay layer rendered behind the alert dialog content. */
-export const AlertDialogOverlay = React.forwardRef<AlertDialogOverlayElement, IAlertDialogOverlayProps>(
-  (props: ScopedProps<IAlertDialogOverlayProps>, forwardedRef) => {
+export const AlertDialogOverlay = React.forwardRef<AlertDialogOverlayElement, IAlertDialog.IOverlayProps>(
+  (props: IAlertDialog.IScoped<IAlertDialog.IOverlayProps>, forwardedRef) => {
     const { __scopeAlertDialog, ...overlayProps } = props
     const dialogScope = useDialogScope(__scopeAlertDialog)
     return <DialogPrimitive.Overlay {...dialogScope} {...overlayProps} ref={forwardedRef} />
--- packages/duck-primitives/src/alert-dialog/trigger.tsx
+++ packages/duck-primitives/src/alert-dialog/trigger.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react'
 import * as DialogPrimitive from '../dialog'
-import { type ScopedProps, useDialogScope } from './alert-dialog'
+import { useDialogScope } from './alert-dialog'
+import type { IAlertDialog } from './alert-dialog.types'
 
 const TRIGGER_NAME = 'AlertDialogTrigger'
 
 type AlertDialogTriggerElement = React.ComponentRef<typeof DialogPrimitive.Trigger>
-type DialogTriggerProps = React.ComponentPropsWithoutRef<typeof DialogPrimitive.Trigger>
-export interface IAlertDialogTriggerProps extends DialogTriggerProps {}
 
 /** Button that opens the alert dialog. */
-export const AlertDialogTrigger = React.forwardRef<AlertDialogTriggerElement, IAlertDialogTriggerProps>(
-  (props: ScopedProps<IAlertDialogTriggerProps>, forwardedRef) => {
+export const AlertDialogTrigger = React.forwardRef<AlertDialogTriggerElement, IAlertDialog.ITriggerProps>(
+  (props: IAlertDialog.IScoped<IAlertDialog.ITriggerProps>, forwardedRef) => {
     const { __scopeAlertDialog, ...triggerProps } = props
     const dialogScope = useDialogScope(__scopeAlertDialog)
     return <DialogPrimitive.Trigger {...dialogScope} {...triggerProps} ref={forwardedRef} />
