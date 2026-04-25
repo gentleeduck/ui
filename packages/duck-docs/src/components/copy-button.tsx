da88bf2c6c3bca4d467ef6f526c703ade6c98938
wildduck <108896341+wildduck2@users.noreply.github.com>
2026-04-25 21:38:41 +0300

    chore(docs): normalize copy button imports

--- packages/duck-docs/src/components/copy-button.tsx
+++ packages/duck-docs/src/components/copy-button.tsx
@@ -1,10 +1,10 @@
 'use client'
 
+import { type Event, trackEvent } from '@duck-docs/lib/events'
 import { cn } from '@gentleduck/libs/cn'
 import { Button, type ButtonProps } from '@gentleduck/registry-ui/button'
 import { CheckIcon, Copy } from 'lucide-react'
 import * as React from 'react'
-import { type Event, trackEvent } from '@duck-docs/lib/events'
 
 export async function copyToClipboardWithMeta(value: string, event?: Event) {
   navigator.clipboard.writeText(value)
